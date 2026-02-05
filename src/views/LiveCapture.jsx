68e98ba838e742b8998855b9b6d94ffd0d2014d8
Brisk4t <atharvapcat@gmail.com>
2026-02-05 13:13:33 -0700

    Crude is fine, im tired

--- src/views/LiveCapture.jsx
+++ src/views/LiveCapture.jsx
@@ -520,13 +520,7 @@ export default function LiveCapture() {
                         // Keyboard event handlers
                         onKeyDown={(e) => {
                             // Default to backspace for unidentified keys to handle mobile keyboard quirks
-                            console.log("KeyDown Event:", e);
-                            if(e.key === "Unidentified") {
-                                handleKeyDown({...e, key: "Backspace", code: "Backspace", keyCode: 8, preventDefault: () => {}});
-                            }
-                            else{
-                                handleKeyDown(e);
-                            }
+                            handleKeyDown(e);
                             mobileInputRef.current.value = "";
                         }}
                         // IME event handlers
